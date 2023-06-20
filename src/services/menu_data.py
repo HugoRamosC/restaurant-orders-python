870849ad4878a6a1cb5803ed500cfec90e3a5a08
HugoRamosC <hugor.castilho@gmail.com>
2023-06-20 20:47:03 -0300

    feat: menuData class - req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,29 @@
+import csv
+from models.ingredient import Ingredient
+from models.dish import Dish
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, encoding="utf-8") as file:
+            menu_reader = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = menu_reader
+            self.header = header
+
+            dishes_dict = dict()
+            for dish_line in data:
+                dish, price, ingredient, recipe_ammount = dish_line
+                if dish in dishes_dict:
+                    dishes_dict[dish].add_ingredient_dependency(
+                        Ingredient(ingredient),
+                        int(recipe_ammount)
+                    )
+                else:
+                    dishes_dict[dish] = (Dish(dish, float(price)))
+                    dishes_dict[dish].add_ingredient_dependency(
+                        Ingredient(ingredient),
+                        int(recipe_ammount)
+                    )
+
+            self.dishes = set(dishes_dict.values())
