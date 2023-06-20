2fb3050e4eb07fcadea85320317ed358ba52d3fd
HugoRamosC <hugor.castilho@gmail.com>
2023-06-19 21:04:32 -0300

    test: ingredient - req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,12 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('ovo')
+    assert ingredient.name == 'ovo'
+    assert ingredient.__hash__() == hash('ovo')
+    assert ingredient.restrictions == {Restriction.ANIMAL_DERIVED}
+    assert ingredient.__eq__(Ingredient('carne')) is False
+    assert ingredient.__eq__(ingredient) is True
+    assert ingredient.__repr__() == "Ingredient('ovo')"
